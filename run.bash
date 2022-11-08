d7ce01a805571d90a68f7ac56cff08bba9d1ed48
freitzzz <1160907@isep.ipp.pt>
2022-11-08 08:10:32 +0000

    play sound along notification if available

--- run.bash
+++ run.bash
@@ -8,6 +8,7 @@ icon_data="0"
 actions='[]'
 hints='{}'
 expire_timeout=0
+sound_path='sounds/complete.oga'
 
 guid=1000
 
@@ -19,7 +20,16 @@ function send_notification() {
         --object-path=/org/freedesktop/Notifications \
         --method=org.freedesktop.Notifications.Notify \
         "$title" "$replace_notification_id" "$icon_data" "$subtitle" "$body" \
-        $actions $hints $expire_timeout
+        $actions "$hints" $expire_timeout
 }
 
-send_notification
+function play_sound() {
+    command=paplay
+    
+    if [[ -x "$(command -v $command)" ]]
+    then
+        $command "$sound_path"
+    fi
+}
+
+send_notification & play_sound
